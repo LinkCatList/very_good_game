bbf55e53688be675415eee6df7924f10668e3e59
linkcatlist <vigalok@bk.ru>
2025-10-26 02:20:50 +0300

    Co-authored-by: Nick Loginov <kemphack@mail.ru>

---
+++ bobrik.gd
@@ -0,0 +1,14 @@
+
+var grid: Array[Array] = []
+var map = '''11111
+10001
+10201
+10001
+11111
+'''
+
+func _ready():
+    pass
+
+func _process(delta):
+    pass
--- new_script.gd
+++ new_script.gd
@@ -1,5 +1,6 @@
-extends Node
+extends RigidBody2D
 
+var SPEED = 132
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,5 +9,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	Input
+	var move_vector = Vector2.ZERO
+	if Input.is_action_just_pressed("move_left"):
+		move_vector.x -= SPEED
+	if Input.is_action_just_pressed("move_right"):
+		move_vector.x += SPEED
+	if Input.is_action_just_pressed("move_up"):
+		move_vector.y -= SPEED
+	if Input.is_action_just_pressed("move_down"):
+		move_vector.y += SPEED
+	
+	var collide_result = move_and_collide(move_vector)
+	if collide_result != null and collide_result.get_collider_shape().get_parent().has_method("move"):
+		collide_result.get_collider_shape().get_parent().move(move_vector)
+		
 	pass
